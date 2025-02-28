bed335917666eabe15ddcaecc3fa0bf619156637
saurabh <bokadesaurabh88@gmail.com>
2025-02-28 15:12:49 +0530

    2nd commit

---
+++ Scientific-Calculator/src/Components/Body/Body.jsx
@@ -0,0 +1,12 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import "./Body.css"
+
+const Body = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Body
---
+++ Scientific-Calculator/src/Components/Head/Head.jsx
@@ -0,0 +1,12 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import "./Head.css"
+
+const Head = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Head
